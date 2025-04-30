ad280d517581436f4869382c2809fbc220f8f2c3
Aditya Jaiswal <32607172+jaiswaladi246@users.noreply.github.com>
2025-05-01 04:12:00 +0530

    Update index.js

--- backend1/index.js
+++ backend1/index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
+// Middleware to log all incoming requests
+app.use((req, res, next) => {
+  const ip = req.headers['x-real-ip'] || req.connection.remoteAddress;
+  console.log(`[Backend1] ${new Date().toISOString()} - ${req.method} ${req.url} from ${ip}`);
+  next();
+});
+
 app.get('/api/', (req, res) => {
   res.send('Response from Backend Server 1');
 });
