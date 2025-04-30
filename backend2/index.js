a63fd8776e4eeb14af50a45e86740bd74daa3575
Aditya Jaiswal <32607172+jaiswaladi246@users.noreply.github.com>
2025-05-01 03:49:06 +0530

    Create index.js

---
+++ backend2/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3002;
+
+app.get('/', (req, res) => {
+  res.send('🟢 Response from Backend Server 2');
+});
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Backend Server 2 running at http://0.0.0.0:${port}`);
+});
